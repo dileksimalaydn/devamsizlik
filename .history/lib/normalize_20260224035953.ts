7e80f639484f9b2e5229b3540f4303cd2dca52cb
dileksimalaydn <d.simalaydin@icloud.com>
2026-02-24 11:57:30 +0300

    Remove .history from repo

--- .history/lib/normalize_20260224035953.ts
+++
@@ -1,7 +0,0 @@
-// lib/normalize.ts
-export function normalizeCourseName(name: string) {
-  return name
-    .trim()
-    .toLocaleLowerCase("tr-TR")
-    .replace(/\s+/g, ""); // boşlukları kaldır (SE 116 == se116)
-}
