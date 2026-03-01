d4496c613e9b93f122faeaa601146089a7d9147b
dileksimalaydn <d.simalaydin@icloud.com>
2026-03-02 00:44:11 +0300

    fix: OG image SVG attribute camelCase düzeltildi

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -44,7 +44,7 @@ export default async function Image() {
               alignItems: "center",
               justifyContent: "center",
             }}>
-              <svg width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <svg width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M22 10v6M2 10l10-5 10 5-10 5z" />
                 <path d="M6 12v5c3 3 9 3 12 0v-5" />
               </svg>
